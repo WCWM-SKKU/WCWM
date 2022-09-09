6282c1c8d4634a14e5a60961af3c5c0c922aafcd
whyjyj <whyjyj0312@gmail.com>
2022-09-09 14:16:32 +0000

    [feat] Recruit Service

--- src/backend/src/main/java/wcwm/wcwm/repository/RecruitRepository.java
+++ src/backend/src/main/java/wcwm/wcwm/repository/RecruitRepository.java
@@ -15,18 +15,18 @@ public class RecruitRepository {
 
     private final EntityManager em;
 
-    List<Recruit> findAll(int firstResult, int maxResults) {
+    public List<Recruit> findAll(int firstResult, int maxResults) {
         return em.createQuery("select r from Recruit r", Recruit.class)
                 .setFirstResult(firstResult)
                 .setMaxResults(maxResults)
                 .getResultList();
     }
 
-    Recruit findById(Long id) {
+    public Recruit findOne(Long id) {
         return em.find(Recruit.class, id);
     }
 
-    List<Recruit> findByMinCareer(int minCareer, int firstResult, int maxResults) {
+    public List<Recruit> findByMinCareer(int minCareer, int firstResult, int maxResults) {
         return em.createQuery("select r from Recruit r where r.min_career >= :minCareer", Recruit.class)
                 .setParameter("minCareer", minCareer)
                 .setFirstResult(firstResult)
@@ -34,7 +34,7 @@ public class RecruitRepository {
                 .getResultList();
     }
 
-    List<Recruit> findByDuty(String duty, int firstResult, int maxResults) {
+    public List<Recruit> findByDuty(String duty, int firstResult, int maxResults) {
         return em.createQuery("select r from Recruit r where r.duty like %:duty%", Recruit.class)
                 .setParameter("duty", duty)
                 .setFirstResult(firstResult)
@@ -42,7 +42,7 @@ public class RecruitRepository {
                 .getResultList();
     }
 
-    List<Recruit> findByMinCareerAndDuty(int minCareer, String duty, int firstResult, int maxResults) {
+    public List<Recruit> findByMinCareerAndDuty(int minCareer, String duty, int firstResult, int maxResults) {
         return em
                 .createQuery("select r from Recruit r where r.min_career >= :minCareer and r.duty like %:duty%",
                         Recruit.class)
